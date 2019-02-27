c6b10755b62a6d052025cd40c276d172ca1c28ba
h0g <rodrigue.geis@icloud.com>
2019-02-27 14:05:01 +0100

    basic GameElement

--- src/GameElement.cpp
+++ src/GameElement.cpp
@@ -7,3 +7,35 @@ double GameElement::getX() const {
 double GameElement::getY() const {
     return position.v.p.y;
 }
+
+void GameElement::setX(double x) {
+    position.v.p.x = x;
+}
+
+void GameElement::setY(double y) {
+    position.v.p.y = y;
+}
+
+sf::Texture &GameElement::getTexture() {
+    return texture;
+}
+
+void GameElement::setTexture(const sf::Texture &t) {
+    texture = t;
+}
+
+Orientation GameElement::getOrientation() const {
+    return orientation;
+}
+
+void GameElement::setOrientation(Orientation orientation) {
+    this->orientation = orientation;
+}
+
+Sommet<VSommet> GameElement::getPosition() {
+    return position;
+}
+
+void GameElement::setPosition(Sommet<VSommet> const &pos) {
+    position = pos;
+}
--- src/GameElement.hpp
+++ src/GameElement.hpp
@@ -5,21 +5,39 @@
 
 #include <SFML/Graphics.hpp>
 
+enum class Orientation {
+    // Basic
+    NORTH, SOUTH, EAST, WEST,
+    // Composed
+    NORTH_EAST, NORTH_WEST,
+    SOUTH_EAST, SOUTH_WEST
+};
 
 class GameElement
 {
 private:
     Sommet<VSommet> position;
     sf::Texture texture;
+    Orientation orientation = Orientation::WEST;
 
 public:
-    GameElement(const Sommet<VSommet> &position, const sf::Texture &texture)
+    GameElement(const Sommet<VSommet> &position,
+                const sf::Texture &texture)
 	: position(position), texture(texture)
     {};
-
-    ~GameElement();
-
+    virtual ~GameElement();
 
     double getX() const;
     double getY() const;
+    void setX(double);
+    void setY(double);
+
+    sf::Texture &getTexture();
+    void setTexture(const sf::Texture &);
+
+    Orientation getOrientation() const;
+    void setOrientation(Orientation);
+
+    Sommet<VSommet> getPosition();
+    void setPosition(Sommet<VSommet> const&);
 };
