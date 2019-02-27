c73c89b4675d28721df3e55e0add496745754759
achrafjhidri <achrafjh@gmail.com>
2019-02-27 11:28:13 +0100

    changemeNT DE Fenetre

--- src/FenetreGrapheSFML.h
+++ src/FenetreGrapheSFML.h
@@ -1,127 +1,381 @@
-#pragma once
-#include <SFML/graphics.hpp>
+#ifndef FENETREGRAPHESFML_H
+#define FENETREGRAPHESFML_H
+
+#include<SFML/Graphics/RenderWindow.hpp>
+#include<SFML/Graphics/VertexArray.hpp>
+//#include <SFML/Graphics/Vertex.hpp>
+#include<SFML/GRAPHICS/CircleShape.hpp>
+#include<SFML/GRAPHICS/ConvexShape.hpp>
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Text.hpp>
+
 #include "Sommet.h"
-#include "Arete.h"
+#include  "Arete.h"
+#include "InfosGrapheDessin.h"
+//#include <InfoSommet.h>
+//#include <InfoArete.h>
 #include "TransfoAffine2D.h"
-#include "VSommet.h"
-#include "Peinture.h"
+
+using namespace sf;
+using namespace std;
+
+/**
+Prend en charge le dessin d'un graphe.
+Suppose que les coordonnï¿½es des sommets sont dï¿½finies par rapport au repï¿½re Monde.
+Prend en charge le passage fenï¿½tre-clï¿½ture
+*/
+
+
+inline const Vector2f vecteur2DToVector2f( const Vecteur2D & v) { float x,y; v.arrondit(x,y); return Vector2f(x,y);}
+
+
+/**
+
+vSommet : en coordonnï¿½es monde
+*/
+inline bool dessinePetitRond(RenderWindow & fenetre, const TransfoAffine2D & t, const VSommet & vSommet, const Font & font,
+							const Vecteur2D & deltaTextePixels = Vecteur2D(VSommet::rayonDisquePixels, VSommet::rayonDisquePixels))
+{
+Vecteur2D position = t.applique(vSommet.p);
+Vecteur2D position1 = position -VSommet::rayonDisquePixels*Vecteur2D(1,1);
+
+// si on appliquait la transfo t globalement ï¿½ tout le graphe avant de commencer ï¿½ dessiner, on optimiserait
+// bcp de calculs !!!!
+
+CircleShape disque((float)VSommet::rayonDisquePixels);
+disque.setFillColor(Color(vSommet.couleur));
+float epaisseurBord = (float)(0.15*VSommet::rayonDisquePixels);
+disque.setOutlineThickness(epaisseurBord);
+disque.setOutlineColor(Color(VSommet::couleurBord));
+
+Vector2f p1 = vecteur2DToVector2f(position1);
+
+disque.setPosition(p1);
+
+fenetre.draw(disque);
+
+Vecteur2D position2 = position - deltaTextePixels;
+unsigned int tailleEnPixels = 17;
+Text texte(vSommet.nom, font,tailleEnPixels);
+texte.setFillColor(Color::Black);
+texte.setPosition(vecteur2DToVector2f(position2));
+fenetre.draw(texte);
+return true;
+}
+
+/**
+dï¿½but et fin sont en coordonnï¿½es monde
+*/
+inline bool dessineSegment( RenderWindow & fenetre, const TransfoAffine2D & t, const unsigned int couleur,
+						const Vecteur2D & debut, const Vecteur2D & fin, const unsigned int epaisseurTraitPixels = 2)
+{
+// on va dessiner un rectangle trï¿½s fin pour reprï¿½senter l'arï¿½te
+
+Vecteur2D A, B;	// {AB] est l'arï¿½te ï¿½ reprï¿½senter
+
+A = t.applique(debut); B = t.applique(fin);		// on passe des coordonnï¿½es monde aux coordonnï¿½es ï¿½cran
+
+Vecteur2D u = B-A;
+
+double nU = norme(u);
+u /=nU;
+Vecteur2D  n = u.rotationDirecteQuartDeTour();
+
+//double e = 1;	// ï¿½paisseur du rectangle = 2*e. longueur du rectangle == AB
+double e = 0.5*epaisseurTraitPixels;	// ï¿½paisseur du rectangle = 2*e. longueur du rectangle == AB
+
+
+Vecteur2D v = e*n;
+
+Vecteur2D A0= A-v,A1 = B-v, A2 = B+v,A3 = A+v; // on calcule les 4 sommets du rectangle dont l'arï¿½te [AB] est un axe de symï¿½trie
+
+Vector2f F0 = vecteur2DToVector2f(A0);
+Vector2f F1 = vecteur2DToVector2f(A1);
+Vector2f F2 = vecteur2DToVector2f(A2);
+Vector2f F3 = vecteur2DToVector2f(A3);
+
+ConvexShape rectangle(4);
+rectangle.setPoint(0,F0);
+rectangle.setPoint(1,F1);
+rectangle.setPoint(2,F2);
+rectangle.setPoint(3,F3);
+
+rectangle.setFillColor(Color(couleur));
+fenetre.draw(rectangle);
+
+return true;
+}
+
 
 /**
 Prend en charge le dessin d'un graphe
-Prend en charge le passage de coordonnées monde ---> écran
+Prend en charge le passage de coordonnï¿½es monde ---> ï¿½cran
 */
-using namespace sf ;
-using namespace std ;
 class FenetreGrapheSFML
 {
 public:
-	RenderWindow fenetre;
-	const Font & font;
+RenderWindow fenetre;
+const Font & font;
 
 
-	TransfoAffine2D t;	// réalise le passage fenêtre-clôture
-					// (ou encore monde vers écran)
+TransfoAffine2D t;	// rï¿½alise le passage fenï¿½tre-clï¿½ture (ou encore monde vers ï¿½cran)
+/**
+Crï¿½e la fenï¿½tre qui va contenir le dessin du graphe.
 
-	/**
-	Crée la fenêtre qui va contenir le dessin du graphe.
+DONNEES : titre : titre de la fenï¿½tre
+		  fondCarte : couleur du fond de la scï¿½ne au formar RGBA
+          coinBG : coin Bas Gauche de la scï¿½ne en coordonnï¿½es monde
+          coinHD : coin Haut Droit de la scï¿½ne en coordonnï¿½es monde
+          largeur : largeur de la fenï¿½tre ï¿½cran (en pixels)
+          hauteur : hauteur de la fenï¿½tre ï¿½cran (en pixels)
+          font : police ï¿½ utiliser pour les annotations sur les sommets
 
-	DONNEES : titre : titre de la fenêtre
-			 fondCarte : couleur du fond de la scène au format RGBA
-			  coinBG : coin Bas Gauche de la scène en coordonnées monde
-			  coinHD : coin Haut Droit de la scène en coordonnées monde
-			  largeur : largeur de la fenêtre écran (en pixels)
-			  hauteur : hauteur de la fenêtre écran (en pixels)
-			  font : police à utiliser pour les annotations sur les sommets
+*/
+FenetreGrapheSFML( const string & titre, const unsigned int fondCarte,
+				const Vecteur2D & coinBG, const Vecteur2D & coinHD,
+								  const int largeur, const int hauteur, const Font & font):
+								  	fenetre(sf::VideoMode(largeur,hauteur), titre), font(font)
+{
+fenetre.clear(Color(fondCarte));
+// calcul de la transformation affine - changement de repï¿½re : monde ---> ï¿½cran
+Vecteur2D coinEcranBG(0,hauteur), coinEcranHD(largeur,0);
+t = TransfoAffine2D::passageMondeEcran(coinBG,coinHD,coinEcranBG,coinEcranHD);
+}
+/**
+Dessine un sommet du graphe. Exemples :  nom, couleur, informations associï¿½es, etc.
+renvoie true en cas de succï¿½s, false en cas d'ï¿½chec
+On suppose que les coordonnï¿½es du sommet sont dï¿½finies par rapport au repï¿½re du monde
+*/
+template <class T>
+bool dessine(const Sommet<T> * sommet) ;
 
-	*/
-	FenetreGrapheSFML(const string & titre,
-		const unsigned int fondCarte,
-		const Vecteur2D & coinBG,
-		const Vecteur2D & coinHD,
-		const int largeur,
-		const int hauteur,
-		const Font & font) :
-		fenetre(sf::VideoMode(largeur, hauteur), titre), font(font)
-	{
-		fenetre.clear(Color(fondCarte));
-		// calcul de la transformation affine - changement de repère : monde ---> écran
-		Vecteur2D coinEcranBG(0, hauteur), coinEcranHD(largeur, 0);
+/**
+Dessine l'arï¿½te (dï¿½but->fin) en utilisant la couleur "couleur"
+renvoie true en cas de succï¿½s, false en cas d'ï¿½chec
+On suppose que les coordonnï¿½es des sommets sont dï¿½finies par rapport au repï¿½re du monde
+*/
+template <class T>
+bool dessine(const Sommet<T> * debut, const Sommet<T> * fin, unsigned int couleur);
 
-		t = TransfoAffine2D::passageMondeEcran(coinBG, coinHD,
-			coinEcranBG, coinEcranHD);
-	}
-	/**
-	Dessine un sommet du graphe. Exemples :  nom, couleur, informations associées, etc.
-	renvoie true en cas de succès, false en cas d'échec
-	On suppose que les coordonnées du sommet sont définies par rapport au repère du monde
-	*/
-	template <class T>
-	bool dessine(const Sommet<T> * sommet);
-
-	/** 
-	Dessine l'arête (début->fin) en utilisant la couleur "couleur"
-	renvoie true en cas de succès, false en cas d'échec
-	On suppose que les coordonnées des sommets sont définies par rapport au repère du monde
-	*/
-	template <class T>
-	bool dessine(const Sommet<T> * debut, const Sommet<T> * fin, unsigned int couleur);
-
-	/**
-	Dessine un arête du graphe. Exemples :  nom, couleur, informations associées, etc.
-	renvoie true en cas de succès, false en cas d'échec
-	On suppose que les coordonnées des sommets sont définies par rapport au repère du monde
-	*/
-	template <class S, class T>
-	bool dessine(const Arete<S, T> * arete);
+/**
+Dessine un arï¿½te du graphe. Exemples :  nom, couleur, informations associï¿½es, etc.
+renvoie true en cas de succï¿½s, false en cas d'ï¿½chec
+On suppose que les coordonnï¿½es des sommets sont dï¿½finies par rapport au repï¿½re du monde
+*/
+template <class S, class T>
+bool dessine(const Arete<S,T> * arete) ;
 
 };
+
 template <>
 bool FenetreGrapheSFML::dessine<VSommet>(const Sommet<VSommet> * sommet)
 {
-	CircleShape s(sommet->v.rayonDisquePixels);
-	s.setFillColor(Color(sommet->v.couleurBord));
-	s.setPosition(Vector2f(sommet->v.p.x, sommet->v.p.y));
-
-	Text t(sommet->v.nom,font,16);
-	t.setPosition(Vector2f(sommet->v.p.x+ sommet->v.rayonDisquePixels, sommet->v.p.y+ sommet->v.rayonDisquePixels));
-	fenetre.draw(s);
-	fenetre.draw(t);
-
-
-	return true ;
-}
-template <>
-bool FenetreGrapheSFML::dessine<VSommet>(const Sommet<VSommet> * debut, const Sommet<VSommet> * fin, unsigned int couleur) {
-
-	Vertex v(sf::Vector2f(fin->v.p.x + fin->v.rayonDisquePixels, fin->v.p.y + fin->v.rayonDisquePixels));
-	Vertex v2(sf::Vector2f(debut->v.p.x + debut->v.rayonDisquePixels, debut->v.p.y + debut->v.rayonDisquePixels));
-
-	v.color = Color(couleur);
-	v2.color = Color(couleur);
-
-
-	Vertex line[] = { v,v2 };
-	
-	fenetre.draw(line, 2, Lines);
-
-	
-	
-	return true;
+return dessinePetitRond(this->fenetre,this->t, sommet->v, this->font);	// mï¿½thode ordinaire. cf. dï¿½but de ce fichier
 }
 
-
-
-template <>
-bool FenetreGrapheSFML::dessine<Peinture, VSommet>(const Arete<Peinture, VSommet> * arete)
+/*
 {
-	dessine(arete->debut, arete->fin, arete->v.fond);
-	dessine(arete->debut, arete->fin, arete->v.devant);
-	dessine(arete->debut);
-	
-	dessine(arete->fin);
+Vecteur2D position = t.applique(sommet->v.p)-VSommet::rayonDisquePixels*Vecteur2D(1,1);
 
+// si on appliquait la transfo t globalement ï¿½ tout le graphe avant de commencer ï¿½ dessiner, on optimiserait
+// bcp de calculs !!!!
 
+CircleShape disque((float)VSommet::rayonDisquePixels);
+disque.setFillColor(Color(sommet->v.couleur));
+float epaisseurBord = (float)(0.15*VSommet::rayonDisquePixels);
+disque.setOutlineThickness(epaisseurBord);
+disque.setOutlineColor(Color(VSommet::couleurBord));
+float x,y;
+position.arrondit(x,y);
 
-	return true ;
+disque.setPosition(x, y);
+
+fenetre.draw(disque);
+
+unsigned int tailleEnPixels = 15;
+Text texte(sommet->v.nom, font,tailleEnPixels);
+texte.setFillColor(Color::Black);
+texte.setPosition(x,y);
+fenetre.draw(texte);
+return true;
+}
+*/
+
+template <>
+bool FenetreGrapheSFML::dessine<Peinture,VSommet>(const Arete<Peinture,VSommet> * arete)
+{
+	   dessineSegment( this->fenetre, this->t,   arete->v.fond, arete->debut->v.p, arete->fin->v.p);
+return dessineSegment( this->fenetre, this->t, arete->v.devant, arete->debut->v.p, arete->fin->v.p);
 }
 
+/*{
+// on va dessiner un rectangle trï¿½s fin pour reprï¿½senter l'arï¿½te
+
+Vecteur2D A(arete->debut->v.p), B(arete->fin->v.p);	// {AB] est l'arï¿½te ï¿½ reprï¿½senter
+
+A = t.applique(A); B = t.applique(B);		// on passe des coordonnï¿½es monde aux coordonnï¿½es ï¿½cran
+
+Vecteur2D u = B-A;
+
+double nU = norme(u);
+u /=nU;
+Vecteur2D  n = u.rotationDirecteQuartDeTour();
+
+double e = 1;	// ï¿½paisseur du rectangle = 2*e. longueur du rectangle == AB
+
+Vecteur2D v = e*n;
+
+Vecteur2D A0= A-v,A1 = B-v, A2 = B+v,A3 = A+v; // on calcule les 4 sommets du rectangle dont l'arï¿½te [AB] est un axe de symï¿½trie
+
+Vector2f F0 = vecteur2DToVector2f(A0);
+Vector2f F1 = vecteur2DToVector2f(A1);
+Vector2f F2 = vecteur2DToVector2f(A2);
+Vector2f F3 = vecteur2DToVector2f(A3);
+
+ConvexShape rectangle(4);
+rectangle.setPoint(0,F0);
+rectangle.setPoint(1,F1);
+rectangle.setPoint(2,F2);
+rectangle.setPoint(3,F3);
+
+rectangle.setFillColor(Color(arete->v.fond));
+fenetre.draw(rectangle);
+rectangle.setFillColor(Color(arete->v.devant));
+fenetre.draw(rectangle);
+
+return true;
+}*/
+
+//---------------------------------------dessine le cas particulier de InfoSommet et InfoArete ------------------------------
+
+/* *
+Dessine un sommet du graphe. Exemples :  nom, couleur, informations associï¿½es, etc.
+renvoie true en cas de succï¿½s, false en cas d'ï¿½chec
+On suppose que les coordonnï¿½es du sommet sont dï¿½finies par rapport au repï¿½re du monde
+* /
+template <>
+bool FenetreGrapheSFML::dessine<InfoSommet>(const Sommet<InfoSommet> * sommet)
+{
+return dessinePetitRond(this->fenetre, this->t, sommet->v.vSommet, this->font);
+}
+
+/ **
+Dessine un arï¿½te du graphe. Exemples :  nom, couleur, informations associï¿½es, etc.
+renvoie true en cas de succï¿½s, false en cas d'ï¿½chec
+On suppose que les coordonnï¿½es des sommets sont dï¿½finies par rapport au repï¿½re du monde
+* /
+template <>
+bool FenetreGrapheSFML::dessine<InfoArete,InfoSommet>(const Arete<InfoArete,InfoSommet> * arete)
+{
+return dessineSegment( this->fenetre, this->t, arete->v.couleur, arete->debut->v.vSommet.p, arete->fin->v.vSommet.p);
+}
+*/
+
+
+//----------------------------------------------------------------------------------
+/**
+par rapport ï¿½ la classe de base, cette fenï¿½tre dessine en plus les axes du repï¿½re monde
+
+*/
+class FenetreGrapheSFMLAvecAxesRepereMonde : public FenetreGrapheSFML
+{
+const Font & font2;
+
+public:
+
+/**
+Crï¿½e la fenï¿½tre qui va contenir le dessin du graphe.
+
+DONNEES : titre : titre de la fenï¿½tre
+		  fondCarte : couleur du fond de la scï¿½ne au formar RGBA
+          coinBG : coin Bas Gauche de la scï¿½ne en coordonnï¿½es monde
+          coinHD : coin Haut Droit de la scï¿½ne en coordonnï¿½es monde
+          largeur : largeur de la fenï¿½tre ï¿½cran (en pixels)
+          hauteur : hauteur de la fenï¿½tre ï¿½cran (en pixels)
+          font1 : police ï¿½ utiliser pour les annotations sur les sommets
+		  font2 : police ï¿½ utiliser pour les annotations sur les axes du repï¿½re
+*/
+
+FenetreGrapheSFMLAvecAxesRepereMonde(const string & titre, unsigned int fondCarte,
+								const Vecteur2D & coinBG, const Vecteur2D & coinHD,
+								  const int largeur, const int hauteur, const Font & font1, const Font & font2):
+							FenetreGrapheSFML( titre, fondCarte, coinBG, coinHD, largeur, hauteur, font1), font2(font2)
+{
+//------------ on va dessiner les axes du repï¿½re avec les annotations usuelles O, I et J -------------
+
+//---------- d'abord on dï¿½finit les points utiles
+
+Vecteur2D O, I(1,0),J(0,1);
+Vecteur2D A0(coinBG.x,0), A1(coinHD.x,0);	// restriction de l'axe des abscisses au rectangle monde
+Vecteur2D B0(0,coinBG.y), B1(0,coinHD.y);	// restriction de l'axe des ordonnï¿½es au rectangle monde
+
+//---------------- on passe des coordonnï¿½es monde ---> ï¿½cran
+
+O = t.applique(O);
+I = t.applique(I);
+J = t.applique(J);
+
+A0 = t.applique(A0);
+A1 = t.applique(A1);
+
+B0 = t.applique(B0);
+B1 = t.applique(B1);
+
+//-------------------- on dessine les 2 axes --------------------------------
+
+VertexArray axeAbscisses(PrimitiveType::Lines);
+
+axeAbscisses.append(Vertex(vecteur2DToVector2f(A0),Color::Black));
+axeAbscisses.append(Vertex(vecteur2DToVector2f(A1),Color::Black));
+
+fenetre.draw(axeAbscisses);
+
+VertexArray axeOrdonnees(PrimitiveType::Lines);
+
+axeOrdonnees.append(Vertex(vecteur2DToVector2f(B0),Color::Black));
+axeOrdonnees.append(Vertex(vecteur2DToVector2f(B1),Color::Black));
+
+fenetre.draw(axeOrdonnees);
+
+//------------------------------ on dessine les points O, I et J
+
+float rayonPointPixels = 4;
+Vecteur2D u(1,1);
+
+Vecteur2D ru = rayonPointPixels*u;
+
+CircleShape O1(rayonPointPixels);
+O1.setFillColor(Color::Red);
+O1.setPosition(vecteur2DToVector2f(O-ru));
+fenetre.draw(O1);
+O1.setPosition(vecteur2DToVector2f(I-ru));
+fenetre.draw(O1);
+O1.setPosition(vecteur2DToVector2f(J-ru));
+fenetre.draw(O1);
+
+//----------------- on dessine les annotations -----------------------
+
+unsigned int tailleEnPixels = 12;
+Text annotation("O", font2,tailleEnPixels);
+annotation.setFillColor(Color::Red);
+double d = 2*rayonPointPixels;
+Vecteur2D v(1,-1);
+annotation.setPosition(vecteur2DToVector2f(O+d*v));
+fenetre.draw(annotation);
+
+annotation.setString("I");
+annotation.setPosition(vecteur2DToVector2f(I+d*v));
+fenetre.draw(annotation);
+
+annotation.setString("J");
+annotation.setPosition(vecteur2DToVector2f(J+d*v));
+fenetre.draw(annotation);
+
+// on  pourrait optimiser les lignes de calcul ci-dessus pour ï¿½viter quelques rï¿½pï¿½titions ----------------
+
+}
+
+};
+
+
+#endif // FENETRE_H
--- src/monMain.cpp
+++ src/monMain.cpp
@@ -8,6 +8,7 @@
 #include "Graphe.h"
 #include "FenetreGrapheSFML.h"
 #include <string>
+#include <vector>
 
 using namespace std;
 using namespace sf;
@@ -25,6 +26,7 @@ int main() {
 	FenetreGrapheSFML f(titre, 10000000,  Vecteur2D(0, 200),  Vecteur2D(300, 0), 760, 800,font);
 	unsigned int magenta = Color::Magenta.toInteger();
 	
+	
 	Graphe<Peinture, VSommet> g1;	// création à vide
 
 
@@ -35,140 +37,15 @@ int main() {
 	Sommet<VSommet> * s15, *s25, *s35, *s45, *s55;
 
 
-	s11 = g1.creeSommet(VSommet("11", Vecteur2D(100, 100), magenta));
-	s21 = g1.creeSommet(VSommet("21", Vecteur2D(250, 100), magenta));
-	s31 = g1.creeSommet(VSommet("31", Vecteur2D(400, 100), magenta));
-	s41 = g1.creeSommet(VSommet("41", Vecteur2D(550, 100), magenta));
-	s51 = g1.creeSommet(VSommet("51", Vecteur2D(700, 100), magenta));
-
-
-	s12 = g1.creeSommet(VSommet("12", Vecteur2D(100, 250), magenta));
-	s22 = g1.creeSommet(VSommet("22", Vecteur2D(250, 250), magenta));
-	s32 = g1.creeSommet(VSommet("32", Vecteur2D(400, 250), magenta));
-	s42 = g1.creeSommet(VSommet("42", Vecteur2D(550, 250), magenta));
-	s52 = g1.creeSommet(VSommet("52", Vecteur2D(700, 250), magenta));
-
-
-	s13 = g1.creeSommet(VSommet("13", Vecteur2D(100, 400), magenta));
-	s23 = g1.creeSommet(VSommet("23", Vecteur2D(250, 400), magenta));
-	s33 = g1.creeSommet(VSommet("33", Vecteur2D(400, 400), magenta));
-	s43 = g1.creeSommet(VSommet("43", Vecteur2D(550, 400), magenta));
-	s53 = g1.creeSommet(VSommet("53", Vecteur2D(700, 400), magenta));
+	s11 = g1.creeSommet(VSommet("11", Vecteur2D(20, 1), magenta));
+	s21 = g1.creeSommet(VSommet("21", Vecteur2D(60, 1), magenta));
+	s31 = g1.creeSommet(VSommet("31", Vecteur2D(100, 1), magenta));
+	s41 = g1.creeSommet(VSommet("41", Vecteur2D(140, 1), magenta));
+	s51 = g1.creeSommet(VSommet("51", Vecteur2D(180, 1), magenta));
 
 
 
-
-	s14 = g1.creeSommet(VSommet("14", Vecteur2D(100, 550), magenta));
-	s24 = g1.creeSommet(VSommet("24", Vecteur2D(250,550), magenta));
-	s34 = g1.creeSommet(VSommet("34", Vecteur2D(400, 550), magenta));
-	s44 = g1.creeSommet(VSommet("44", Vecteur2D(550, 550), magenta));
-	s54 = g1.creeSommet(VSommet("54", Vecteur2D(700, 550), magenta));
-
-
-	s15 = g1.creeSommet(VSommet("15", Vecteur2D(100, 700), magenta));
-	s25 = g1.creeSommet(VSommet("25", Vecteur2D(250, 700), magenta));
-	s35 = g1.creeSommet(VSommet("35", Vecteur2D(400, 700), magenta));
-	s45 = g1.creeSommet(VSommet("45", Vecteur2D(550, 700), magenta));
-	s55 = g1.creeSommet(VSommet("55", Vecteur2D(700, 700), magenta));
-
-
-
-	//----------------- on connecte certains sommets -------------------
-
-	Arete<Peinture, VSommet> * a1112, *a1213, *a1314, *a1415;
-	Arete<Peinture, VSommet> * a2122, *a2223, *a2324, *a2425;
-	Arete<Peinture, VSommet> * a3132, *a3233, *a3334, *a3435;
-	Arete<Peinture, VSommet> * a4142, *a4243, *a4344, *a4445;
-	Arete<Peinture, VSommet> * a5152, *a5253, *a5354, *a5455;
-
-
-
-	a1112 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s11, s12);
-	a1213 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s12, s13);
-	a1314 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s13, s14);
-	a1415 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s14, s15);
-
-	a2122 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s21, s22);
-	a2223 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s22, s23);
-	a2324 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s23, s24);
-	a2425 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s24, s25);
-
-	a3132 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s31, s32);
-	a3233 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s32, s33);
-	a3334 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s33, s34);
-	a3435 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s34, s35);
-
-
-	a4142 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s41, s42);
-	a4243 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s42, s43);
-	a4344 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s43, s44);
-	a4445 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s44, s45);
-
-
-	a5152 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s51, s52);
-	a5253 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s52, s53);
-	a5354 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s53, s54);
-	a5455 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s54, s55);
-
-
-	Arete<Peinture, VSommet> * a1121, *a2131, *a3141, *a4151;
-	Arete<Peinture, VSommet> * a1222, *a2232, *a3242, *a4252;
-	Arete<Peinture, VSommet> * a1323, *a2333, *a3343, *a4353;
-	Arete<Peinture, VSommet> * a1424, *a2434, *a3444, *a4454;
-	Arete<Peinture, VSommet> * a1525, *a2535, *a3545, *a4555;
-
-	a1121 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s11, s21);
-	a2131 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s21, s31);
-	a3141= g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s31, s41);
-	a4151 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s41, s51);
-
-	a1222 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s12, s22);
-	a2232 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s22, s32);
-	a3242 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s32, s42);
-	a4252 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s42, s52);
-
-
-
-	a1323 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s13, s23);
-	a2333 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s23, s33);
-	a3343 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s33, s43);
-	a4353 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s43, s53);
-
-	a1424 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s14, s24);
-	a2434 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s24, s34);
-	a3444 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s34, s44);
-	a4454 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s44, s54);
-
-	a1525 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s15, s25);
-	a2535 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s25, s35);
-	a3545 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s35, s45);
-	a4555 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s45, s55);
-
-	
-	Arete<Peinture, VSommet> * a1122, *a1223, *a1423, *a1524;
-	Arete<Peinture, VSommet> * a2132, *a2233, *a2433, *a2534;
-	Arete<Peinture, VSommet> * a3241, *a3342, *a3344, *a3445;
-	Arete<Peinture, VSommet> * a4251, *a4352, *a4354, *a4455;
-	
-	a1122 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s11, s22);
-	a1223 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s12, s23);
-	a1423 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s14, s23);
-	a1524 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s15, s24);
-
-	a2132= g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s21, s32);
-	a2233 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s22, s33);
-	a2433 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s24, s33);
-	a2534 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s25, s34);
-
-	a3241 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s32, s41);
-	a3342 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s33, s42);
-	a3344 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s33, s44);
-	a3445 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s34, s45);
-
-	a4251 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s42, s51);
-	a4352 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s43, s52);
-	a4354 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s43, s54);
-	a4455 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s44, s55);
+	a4455 = g1.creeArete(Peinture(Color::Blue.toInteger(), Color::Red.toInteger()), s41, s11);
 
 
 
