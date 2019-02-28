e94a4136db4648e1c0d8cc6fad1171a80452ad24
achrafjhidri <achrafjh@gmail.com>
2019-02-28 17:47:49 +0100

    ajout du AnimatedSprite.cpp

---
+++ src/AnimatedSprite.cpp
@@ -0,0 +1,28 @@
+#include "AnimatedSprite.hpp"
+
+
+
+AnimatedSprite::AnimatedSprite(const sf::Texture & textureSheet,int largeur,int hauteur,int nbTextures)
+    :Sprite(),it(cadres.begin())
+{
+    for (int i = 0 ; i < nbTextures;i++)
+        cadres.push_back(sf::IntRect(i*largeur,0,i*largeur+largeur,hauteur));
+    setSprite(textureSheet,sf::IntRect(*it));
+
+}
+
+AnimatedSprite::~AnimatedSprite(){};
+// faut petre rajouter un parametre en entré Orientaion (l'enum LEFT.....)
+// comme ça on sais si on doit faire une rotation de la texture
+void AnimatedSprite::update(){
+    sf::Clock clock;
+    if ( clock.getElapsedTime().asSeconds() > 0.5f )
+        {
+            if (it == cadres.end() )
+                it=cadres.begin();
+            else 
+                it++;
+
+            setSprite.setTextureRect(*it);
+        }
+}
--- src/AnimatedSprite.hpp
+++ src/AnimatedSprite.hpp
@@ -1,4 +1,19 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <list>
+using namespace std ;
 
+// a faire
+class AnimatedSprite : class Sprite {
+
+    list <sf::IntRect> cadres ;
+    list <sf::IntRect>::iterator it ;
+  
+public:
+  AnimatedSprite(const sf::Texture & textureSheet,int largeur,int hauteur,int nbTextures);
+  ~AnimatedSprite();
+
+  void update ();
+  void move( const sf::Vector2f &  );
+}
