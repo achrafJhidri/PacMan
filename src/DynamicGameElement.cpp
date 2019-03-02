13b214ad871819937d20bf2a77380daa26363a12
achrafjhidri <achrafjh@gmail.com>
2019-03-02 17:02:03 +0100

    factorisation du code dans move de dynamic element

--- src/DynamicGameElement.cpp
+++ src/DynamicGameElement.cpp
@@ -17,101 +17,68 @@ void DynamicGameElement::setSpeed(double s) {
 
 void DynamicGameElement::move( Orientation orientation ){
         if ( orientation == Orientation::SOUTH){
-
-            PElement<Sommet<VSommet>> * copieListVoisin =  PElement<Sommet<VSommet>>::copie1(position.listVoisin);
-            while(copieListVoisin ){    
-                if (( copieListVoisin->v->v.p.y > position.v.p.y  ) && (copieListVoisin->v->v.p.x == position.v.p.x) ){
-                    position=*(copieListVoisin->v);
-                    sprite.setRotation(90);
-                    copieListVoisin = NULL;
-                }else 
-                    copieListVoisin=copieListVoisin->s;
-            }}
+               positionRelative(*this,90,estAuSud);
+            }
         if ( orientation == Orientation::NORTH){
-            PElement<Sommet<VSommet>> * copieListVoisin =  PElement<Sommet<VSommet>>::copie1(position.listVoisin);
-   
-            while(copieListVoisin  ){
-            
-                if ( (copieListVoisin->v->v.p.y  < position.v.p.y )  && (copieListVoisin->v->v.p.x == position.v.p.x) ){
-                        
-                    position=*(copieListVoisin->v);
-                    sprite.setRotation(-90);
-                    copieListVoisin = NULL;
-                }else 
-                    copieListVoisin=copieListVoisin->s;
-            }}
+               positionRelative(*this,-90,estAuNord);
+            }
         if ( orientation == Orientation::EAST){
-            PElement<Sommet<VSommet>> * copieListVoisin =  PElement<Sommet<VSommet>>::copie1(position.listVoisin);
-            
-            while(copieListVoisin ){
-            
-                    if ( (copieListVoisin->v->v.p.x  > position.v.p.x ) && (copieListVoisin->v->v.p.y == position.v.p.y) ){
-                       
-                        position=*(copieListVoisin->v);
-                        sprite.setRotation(0);
-                        copieListVoisin = NULL;
-                    }else 
-                        copieListVoisin=copieListVoisin->s;
-            }}
+            positionRelative(*this,0,estAuEst);
+            }
         if ( orientation == Orientation::WEST){
-            PElement<Sommet<VSommet>> * copieListVoisin =  PElement<Sommet<VSommet>>::copie1(position.listVoisin);
-           
-            while(copieListVoisin ){
-                if ( (copieListVoisin->v->v.p.x < position.v.p.x) &&  (copieListVoisin->v->v.p.y == position.v.p.y) ){
-                    
-                    position=*(copieListVoisin->v);
-                    sprite.setRotation(-180);
-                     copieListVoisin = NULL;
-                }else 
-                    copieListVoisin=copieListVoisin->s;
-            }}
+             positionRelative(*this,180,estAuOuest);
+            }
         if ( orientation == Orientation::NORTH_EAST){
-            PElement<Sommet<VSommet>> * copieListVoisin =  PElement<Sommet<VSommet>>::copie1(position.listVoisin);
-           
-            while(copieListVoisin ){
-                if ( (copieListVoisin->v->v.p.x > position.v.p.x) &&  (copieListVoisin->v->v.p.y < position.v.p.y) ){
-                    
-                    position=*(copieListVoisin->v);
-                    sprite.setRotation(-45);
-                     copieListVoisin = NULL;
-                }else 
-                    copieListVoisin=copieListVoisin->s;
-            }}
+             positionRelative(*this,-45,estAuNordEst);
+          }
         if ( orientation == Orientation::NORTH_WEST){
-            PElement<Sommet<VSommet>> * copieListVoisin =  PElement<Sommet<VSommet>>::copie1(position.listVoisin);
-           
-            while(copieListVoisin ){
-                if ( (copieListVoisin->v->v.p.x < position.v.p.x) &&  (copieListVoisin->v->v.p.y < position.v.p.y) ){
-                    
-                    position=*(copieListVoisin->v);
-                    sprite.setRotation(-135);
-                     copieListVoisin = NULL;
-                }else 
-                    copieListVoisin=copieListVoisin->s;
-            }}
+            positionRelative(*this,-135,estAuNordOuest);}
         if ( orientation == Orientation::SOUTH_EAST){
-            PElement<Sommet<VSommet>> * copieListVoisin =  PElement<Sommet<VSommet>>::copie1(position.listVoisin);
-           
-            while(copieListVoisin ){
-                if ( (copieListVoisin->v->v.p.x > position.v.p.x) &&  (copieListVoisin->v->v.p.y > position.v.p.y) ){
-                    
-                    position=*(copieListVoisin->v);
-                    sprite.setRotation(45);
-                     copieListVoisin = NULL;
-                }else 
-                    copieListVoisin=copieListVoisin->s;
-            }}
+             positionRelative(*this,45,estAuSudEst);}
         if ( orientation == Orientation::SOUTH_WEST){
-            PElement<Sommet<VSommet>> * copieListVoisin =  PElement<Sommet<VSommet>>::copie1(position.listVoisin);
-           
-            while(copieListVoisin ){
-                if ( (copieListVoisin->v->v.p.x < position.v.p.x) &&  (copieListVoisin->v->v.p.y > position.v.p.y) ){
-                    
-                    position=*(copieListVoisin->v);
-                    sprite.setRotation(135);
-                     copieListVoisin = NULL;
-                }else 
-                    copieListVoisin=copieListVoisin->s;
-            }}
+             positionRelative(*this,135,estAuSudOuest);}
 }
 
+
+
+void DynamicGameElement::positionRelative( DynamicGameElement & element,const double deg,bool (*checkOrientationFunction)(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a)){
+    PElement<Sommet<VSommet>> * copieListVoisin =  PElement<Sommet<VSommet>>::copie1(position.listVoisin);
+            while(copieListVoisin ){    
+                if ( checkOrientationFunction(*(copieListVoisin->v),position) ){
+                    position=*(copieListVoisin->v);
+                    sprite.setRotation(deg);
+                    copieListVoisin = NULL;
+                }else 
+                    copieListVoisin=copieListVoisin->s;
+            }
+         PElement<Sommet<VSommet>>::efface1(copieListVoisin);
+}
+
+/*static*/ bool DynamicGameElement::estAuNord(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a) // renvoi true si le voisin est au nord de a cad  y du voisin < au y de a 
+{    return ( voisin.v.p.y  < a.v.p.y ) &&  memeX(voisin,a) ;}
+
+/*static*/ bool DynamicGameElement::memeX(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a) // renvoi true si ils ont le meme x
+{    return ( voisin.v.p.x == a.v.p.x ) ;}
+/*static*/ bool DynamicGameElement::memeY(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a) // renvoi true si ils ont le meme x
+{    return ( voisin.v.p.y == a.v.p.y ) ;}
+
+/*static*/ bool DynamicGameElement::estAuSud(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a) // renvoi true si le voisin est au sud de a cad  y du voisin au > y de a 
+{   return ( voisin.v.p.y  > a.v.p.y ) &&  memeX(voisin,a) ;}
+
+/*static*/ bool DynamicGameElement::estAuEst(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a) // renvoi true si le voisin est au nord de a cad  x du voisin > au x de a 
+{   return ( voisin.v.p.x  > a.v.p.x ) &&  memeY(voisin,a) ;}
+
+/*static*/ bool DynamicGameElement::estAuOuest(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a) // renvoi true si le voisin est au nord de a cad  x du voisin < au x de a 
+{    return ( voisin.v.p.x  < a.v.p.x ) &&  memeY(voisin,a) ;}
+
+
+/*static*/ bool DynamicGameElement::estAuNordEst(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a) // renvoi true si le voisin est au nord est
+{    return ( voisin.v.p.y  < a.v.p.y ) &&  ( voisin.v.p.x  > a.v.p.x) ;}
+
+/*static*/ bool DynamicGameElement::estAuNordOuest(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a) // renvoi true si le voisin est au nord ouest
+{    return ( voisin.v.p.y  < a.v.p.y ) &&  ( voisin.v.p.x  < a.v.p.x) ;}
+
+/*static*/ bool DynamicGameElement::estAuSudEst(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a) // renvoi true si le voisin est au sud Est
+{    return ( voisin.v.p.y  > a.v.p.y ) &&  ( voisin.v.p.x  > a.v.p.x) ;}
+/*static*/ bool DynamicGameElement::estAuSudOuest(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a) // renvoi true si le voisin est au sud Ouest
+{    return ( voisin.v.p.y  > a.v.p.y ) &&  ( voisin.v.p.x  < a.v.p.x) ;}
--- src/DynamicGameElement.hpp
+++ src/DynamicGameElement.hpp
@@ -15,4 +15,18 @@ public:
 
     void move( Orientation orientation );
 
+    void positionRelative( DynamicGameElement & element,const double deg,bool (*checkOrientationFunction)(const Sommet<VSommet> & a,const Sommet<VSommet> & voisin));
+
+    static bool estAuNord(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a);
+    static bool estAuSud(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a);
+    static bool estAuEst(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a);
+    static bool estAuOuest(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a);
+    static bool memeX(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a);
+    static bool memeY(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a);
+    static bool estAuNordEst(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a);
+    static bool estAuNordOuest(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a);
+
+    static bool estAuSudEst(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a);
+    static bool estAuSudOuest(const Sommet<VSommet> & voisin,const Sommet<VSommet> & a);
+
 };
